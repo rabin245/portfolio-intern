a2f72262c17d3bc4d10041b0b5dcfc6b07f2992d
rabin245 <drarabin@gmail.com>
2023-04-10 18:04:35 +0545

    add card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
+import Card from "../../components/Card";
 import styles from "./Home.module.css";
 
 const Home = ({ homeRef }) => {
   return (
     <div className={styles.Home} ref={homeRef}>
       Home page
+      <Card />
     </div>
   );
 };
