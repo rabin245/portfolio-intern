882356221645f868797d272f67810c3b88c7bb07
rabin245 <drarabin@gmail.com>
2023-04-11 14:27:38 +0545

    add about section contents

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -3,7 +3,20 @@ import styles from "./About.module.css";
 const About = ({ aboutRef }) => {
   return (
     <div className={styles.About} ref={aboutRef}>
-      About section
+      <div className={styles.wrapper}>
+        <div className={styles.experience}>
+          <h2>1+</h2>
+          <span>years experience in web development</span>
+        </div>
+
+        <div className={styles.description}>
+          <div className={styles.content}>
+            <span>Full Stack Web Developer</span> specialized in ReactJS and
+            NodeJS
+          </div>
+          <div className={styles.cards}>cards</div>
+        </div>
+      </div>
     </div>
   );
 };
