48f892a69b7fc857d4a553e990cc189099953198
rabin245 <drarabin@gmail.com>
2023-04-11 11:11:16 +0545

    updated the theme context to props

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import MenuItem from "../MenuItem";
 import ThemeSwitcher from "../ThemeSwitcher";
 import styles from "./Menu.module.css";
+import { ThemeContext } from "../../context/ThemeContext";
 
 const Menu = ({ homeRef, aboutRef, contactRef, skillsRef, projectsRef }) => {
+  const { darkMode, switchTheme } = useContext(ThemeContext);
+
   return (
     <div className={styles.menu}>
       <MenuItem title="Home" reference={homeRef} />
@@ -10,7 +14,7 @@ const Menu = ({ homeRef, aboutRef, contactRef, skillsRef, projectsRef }) => {
       <MenuItem title="Skills" reference={skillsRef} />
       <MenuItem title="Projects" reference={projectsRef} />
       <MenuItem title="Contact" reference={contactRef} />
-      <ThemeSwitcher />
+      <ThemeSwitcher darkMode={darkMode} switchTheme={switchTheme} />
     </div>
   );
 };
--- src/components/ThemeSwitcher/index.jsx
+++ src/components/ThemeSwitcher/index.jsx
@@ -1,12 +1,8 @@
 import SunIcon from "../../assets/icons/SunIcon";
 import MoonIcon from "../../assets/icons/MoonIcon";
-import { useContext } from "react";
-import { ThemeContext } from "../../context/ThemeContext";
 import styles from "./ThemeSwitcher.module.css";
 
-const ThemeSwitcher = () => {
-  const { darkMode, switchTheme } = useContext(ThemeContext);
-
+const ThemeSwitcher = ({ darkMode, switchTheme }) => {
   const icon = darkMode ? <MoonIcon /> : <SunIcon />;
 
   return (
