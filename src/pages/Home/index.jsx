75ab49232517e2821e98b608c656a9cfec9f1d3f
rabin245 <drarabin@gmail.com>
2023-04-11 11:43:42 +0545

    add banner in home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,28 @@
-import Card from "../../components/Card";
 import styles from "./Home.module.css";
+import Image from "../../assets/images/banner-image.png";
+import Button from "../../components/Button";
 
 const Home = ({ homeRef }) => {
   return (
     <div className={styles.Home} ref={homeRef}>
-      Home page
-      <Card />
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <h1>
+            Hi, I'm <span className={styles.name}>Rabindra Baisnab</span>
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor odio
+            ad nisi harum fugiat hic aliquam fugit repudiandae ea iusto corporis
+            pariatur ullam deleniti culpa perspiciatis, odit aperiam esse omnis.
+          </p>
+          <div>
+            <Button>Hire me</Button>
+          </div>
+        </div>
+        <div className={styles.image}>
+          <img src={Image} alt="banner" />
+        </div>
+      </div>
     </div>
   );
 };
