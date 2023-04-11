06e45d3eb8a1db3ed09560af72a59c0a79eae3b3
rabin245 <drarabin@gmail.com>
2023-04-11 16:40:18 +0545

    add skill cards to skills page

--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -1,9 +1,49 @@
+import SkillCard from "../../components/SkillCard";
 import styles from "./Skills.module.css";
 
 const Skills = ({ skillsRef }) => {
+  const skills = [
+    {
+      title: "HTML",
+      completed: 80,
+    },
+    {
+      title: "CSS",
+      completed: 70,
+    },
+    {
+      title: "JavaScript",
+      completed: 60,
+    },
+    {
+      title: "Python",
+      completed: 80,
+    },
+    {
+      title: "React",
+      completed: 60,
+    },
+    {
+      title: "Node",
+      completed: 60,
+    },
+  ];
+
   return (
     <div className={styles.Skills} ref={skillsRef}>
-      Skills section
+      <div className={styles.wrapper}>
+        <h1>Skills</h1>
+
+        <div className={styles.cards}>
+          {skills.map((skill) => (
+            <SkillCard
+              key={skill.title}
+              title={skill.title}
+              completed={skill.completed}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
