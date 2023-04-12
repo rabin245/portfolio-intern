630da959aa3ef6d57c90ca8b3e82f9cd5739d20c
rabin245 <drarabin@gmail.com>
2023-04-12 22:39:01 +0545

    add arrow up icon

---
+++ src/assets/icons/ArrowUpIcon.jsx
@@ -0,0 +1,21 @@
+const ArrowUpIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="icon icon-tabler icon-tabler-arrow-up"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    strokeWidth={1.75}
+    stroke="currentColor"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M12 5l0 14"></path>
+    <path d="M18 11l-6 -6"></path>
+    <path d="M6 11l6 -6"></path>
+  </svg>
+);
+
+export default ArrowUpIcon;
