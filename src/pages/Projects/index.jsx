500bc8d915de3422e83c71a0e608519fc4cff38f
rabin245 <drarabin@gmail.com>
2023-04-12 17:38:16 +0545

    add header section

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -4,6 +4,7 @@ import ElectionDappImage from "../../assets/images/project-electiondapp.png";
 import BlogImage from "../../assets/images/project-blogapp.png";
 import QuizImage from "../../assets/images/project-quizzical.png";
 import { ProjectCard } from "../../components/Card";
+import ArrowRightIcon from "../../assets/icons/ArrowRightIcon";
 
 const Projects = ({ projectsRef }) => {
   const projects = [
@@ -34,8 +35,27 @@ const Projects = ({ projectsRef }) => {
   return (
     <div className={styles.Projects} ref={projectsRef}>
       <div className={styles.wrapper}>
-        <div>
+        <div className={styles.header}>
           <h1 className={styles.title}>Recent works</h1>
+
+          <div className={styles.content}>
+            <p className={styles.description}>
+              Here are some of my recent projects. I have also worked on some
+              other projects which are not listed here. You can find them on my
+              GitHub profile.
+            </p>
+            <a
+              href="https://github.com/rabin245"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.link}
+            >
+              View on GitHub
+              <span className={styles.arrow}>
+                <ArrowRightIcon />
+              </span>
+            </a>
+          </div>
         </div>
 
         <div className={styles.projects}>
