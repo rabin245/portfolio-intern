f30a3bf1d76daaff48ab7c57133d6b99e12652af
rabin245 <drarabin@gmail.com>
2023-04-12 13:59:22 +0545

    add contact form and info in the contact section

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,9 +1,113 @@
+import { useState } from "react";
 import styles from "./Contact.module.css";
+import PhoneIcon from "../../assets/icons/PhoneIcon";
+import MailIcon from "../../assets/icons/MailIcon";
+import MapIcon from "../../assets/icons/MapIcon";
+import FindMeIcon from "../../assets/icons/FindMeIcon";
+import FormInput from "../../components/FormInput";
+import Button from "../../components/Button";
+import ContactInfo from "../../components/ContactInfo";
+
+const ContactForm = ({ inputs, handleChange, handleSubmit }) => {
+  return (
+    <div className={styles.ContactForm}>
+      <form action="">
+        <FormInput
+          fieldName="Name"
+          name="name"
+          value={inputs.name}
+          handleChange={handleChange}
+        />
+        <FormInput
+          fieldName="Email"
+          name="email"
+          type="email"
+          value={inputs.email}
+          handleChange={handleChange}
+        />
+        <FormInput
+          fieldName="Message"
+          name="message"
+          value={inputs.message}
+          isMessage={true}
+          handleChange={handleChange}
+        />
+
+        <div className={styles.button}>
+          <Button handleClick={handleSubmit}>Send</Button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 const Contact = ({ contactRef }) => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    message: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+
+    name = name.toLowerCase();
+
+    setInputs((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Submitted", inputs);
+  };
+
   return (
     <div className={styles.Contact} ref={contactRef}>
-      Contact page
+      <div className={styles.wrapper}>
+        <div className={styles.connectInfo}>
+          <h1>Connect with me</h1>
+
+          <ContactInfo
+            icon={<PhoneIcon size={40} />}
+            title="Call me"
+            link="tel:+9779762875880"
+            linkText="+977-9762875880"
+          />
+
+          <ContactInfo
+            icon={<MailIcon size={40} />}
+            title="Email me"
+            link="mailto:drarabin@gmail.com"
+            linkText="drarabin@gmail.com"
+          />
+
+          <ContactInfo
+            icon={<FindMeIcon size={40} />}
+            title="Find me on"
+            link="https://www.linkedin.com/in/rabindra-baisnab/"
+            linkText="Linkedin"
+          />
+
+          <ContactInfo
+            icon={<MapIcon size={40} />}
+            title="Address"
+            link="https://goo.gl/maps/N9qLMN2MeFWjK5UF6"
+            linkText="Chundevi, Bhaktapur"
+          />
+        </div>
+
+        <ContactForm
+          inputs={inputs}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+        />
+      </div>
     </div>
   );
 };
