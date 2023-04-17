06d949cde8938c99d1dc6643e6739e3fe30f817d
rabin245 <drarabin@gmail.com>
2023-04-17 16:51:08 +0545

    restructure card components

---
+++ src/components/Card/CardInfo.jsx
@@ -0,0 +1,17 @@
+import styles from "./Card.module.css";
+
+const ContactInfo = ({ icon, title, link, linkText }) => {
+  return (
+    <div className={styles.infoDiv}>
+      {icon}
+      <div className={styles.info}>
+        <span>{title}</span>
+        <a href={link} target="_blank" rel="noreferrer">
+          {linkText}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
