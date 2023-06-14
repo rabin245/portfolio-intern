04742f595397a93238200a0bfc9d363e2b9d8e5e
rabin245 <drarabin@gmail.com>
2023-06-14 07:21:47 +0545

    feat: add animated image in homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ import DownloadIcon from "../../assets/icons/DownloadIcon";
 import data from "../../assets/data/data.json";
 import { useState, useEffect } from "react";
 import loader from "../../utils/IconLoader";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 const Home = ({ homeRef, contactRef }) => {
   const scrollToRef = (ref) =>
@@ -45,7 +46,12 @@ const Home = ({ homeRef, contactRef }) => {
           </div>
         </div>
         <div className={styles.image}>
-          <img src={Image} alt="banner" />
+          <Player
+            src="https://assets1.lottiefiles.com/packages/lf20_xmkgn4jj.json"
+            speed={0.4}
+            autoplay
+            loop
+          ></Player>
         </div>
       </div>
     </div>
